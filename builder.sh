f4576b653dcb5eb3d804dd8bee7d4215b262d695
comeonjy <1126254578@qq.com>
2019-09-27 19:56:34 +0800

    âdockerfile

--- builder.sh
+++ builder.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 make build
+touch Dockerfile
+echo -e "FROM golang:latest" >> Dockerfile
+echo -e "COPY ./bin/main.exe /" >> Dockerfile
+echo -e "COPY run.cmd /" >> Dockerfile
+echo -e "CMD[\"/run.cmd\"]" >> Dockerfile
+
+docker build
