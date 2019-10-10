002372437ed1a26f0dc59c5b38dc708ea819355f
comeonjy <1126254578@qq.com>
2019-10-10 23:14:20 +0800

    1

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM golang:latest
+WORKDIR $GOPATH
+COPY ./src $GOPATH
+COPY run.cmd $GOPATH
+RUN go build .
+CMD ["./src.exe"]
--- builder.sh
+++ builder.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 make build
-touch Dockerfile
-echo -e "FROM golang:latest" >> Dockerfile
-echo -e "WORKDIR /" >> Dockerfile
-echo -e "COPY ./bin/main.exe /" >> Dockerfile
-echo -e "COPY run.cmd /" >> Dockerfile
-echo -e "CMD [\"./main.exe\"]" >> Dockerfile
+#rm Dockerfile
+#touch Dockerfile
+#echo -e "FROM golang:latest" >> Dockerfile
+#echo -e "WORKDIR $GOPATH" >> Dockerfile
+#echo -e "COPY ./src $GOPATH" >> Dockerfile
+#echo -e "COPY run.cmd $GOPATH" >> Dockerfile
+#echo -e "RUN go build ." >> Dockerfile
+#echo -e "CMD [\"./src.exe\"]" >> Dockerfile
 docker build -t j_one .
