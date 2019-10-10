fe4cdec45610b8c25c72307b938f879306a68e50
comeonjy <1126254578@qq.com>
2019-10-10 21:40:43 +0800

    t

--- builder.sh
+++ builder.sh
@@ -2,8 +2,8 @@
 make build
 touch Dockerfile
 echo -e "FROM golang:latest" >> Dockerfile
-echo -e "WORKDIR $GOPATH" >> Dockerfile
-echo -e "COPY ./bin/main.exe $GOPATH" >> Dockerfile
-echo -e "COPY run.cmd $GOPATH" >> Dockerfile
+echo -e "WORKDIR /" >> Dockerfile
+echo -e "COPY ./bin/main.exe /" >> Dockerfile
+echo -e "COPY run.cmd /" >> Dockerfile
 echo -e "CMD [\"./main.exe\"]" >> Dockerfile
 docker build -t j_one .
