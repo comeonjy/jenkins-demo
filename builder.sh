7e196636eeaeba7edb0c14bfb091edb0891bbadb
comeonjy <1126254578@qq.com>
2019-10-18 00:26:22 +0800

    1

--- builder.sh
+++ builder.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -i -o ./bin/main src/main.go
 rm Dockerfile
-echo -e "FROM golang:latest" >> Dockerfile
-echo -e "WORKDIR /" >> Dockerfile
-echo -e "COPY ./bin/main /" >> Dockerfile
-echo -e "EXPOSE 1234" >> Dockerfile
-echo -e  "CMD [\"./main\"]" >> Dockerfile
+echo "FROM golang:latest" >> Dockerfile
+echo "WORKDIR /" >> Dockerfile
+echo "COPY ./bin/main /" >> Dockerfile
+echo "EXPOSE 1234" >> Dockerfile
+echo  "CMD [\"./main\"]" >> Dockerfile
 ls -a
 docker build -t j_one .
