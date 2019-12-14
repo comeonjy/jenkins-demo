5cacec51ea00f3859fc4498a843c6421d2889067
comeonjy <1126254578@qq.com>
2019-12-14 18:26:10 +0800

    test

---
+++ src/learn/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+type A struct {
+	code int
+}
+
+func (a *A) T1() {
+	fmt.Println("a t1")
+}
+
+func (a *A) T2() {
+	fmt.Println("a t2")
+}
+
+type B struct {
+	BA A
+}
+
+func (b *B) T3()  {
+	fmt.Println("b t2")
+}
+
+func main() {
+	b:=&B{}
+	b.BA.T1()
+	b.BA.T2()
+}
